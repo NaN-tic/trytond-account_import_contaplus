d8d8857d860b0d0139a2de72c14aeae923a4e85c
francesc <francesc@localhost>
2016-03-16 10:22:16 +0100

    raise errors, check balance, special cases

--- account.py
+++ account.py
@@ -79,6 +79,20 @@ def read(data):
     return filter_with_account(read_all(data))
 
 
+def add_tupla2(t1, t2):
+    return (t1[0] + t2[0], t1[1] + t2[1])
+
+
+def not_balance(move):
+    credit_debit = reduce(
+        lambda t_cd, line:
+        add_tupla2(t_cd, (line.credit, line.debit)),
+        move.lines,
+        [0,0])
+    print (move.number)
+    print (credit_debit)
+    return credit_debit[0] != credit_debit[1]
+
 class AccountImportContaplusStart(ModelView):
     'Account Import Contaplus Start'
     __name__ = 'account.import.contaplus.start'
@@ -89,6 +103,8 @@ class AccountImportContaplusStart(ModelView):
     def default_journal():
         Journal = Pool().get('account.journal')
         return Journal.search([('type', '=', 'general')], limit=1)[0].id
+
+
 
 
 class AccountImportContaplus(Wizard):
@@ -105,7 +121,10 @@ class AccountImportContaplus(Wizard):
     def __setup__(cls):
         super(AccountImportContaplus, cls).__setup__()
         cls._error_messages.update({
-                'duplicated move number': ('Duplicated account move number "%(move_number)s".'),
+                'number exists': ('Duplicated account move number "%(move_number)s".'),
+                'account not found': ('Account "%(account)s" not found '),
+                'party not found': ('Party "%(party)s" not found '),
+                'unbalance lines': ('Unbalance lines')
                 })
 
     def transition_import_(self):
@@ -127,10 +146,8 @@ class AccountImportContaplus(Wizard):
                 move.number = iline.asien
 
                 if len(Move.search(['number', '=', move.number], limit=1)) > 0:
-                    self.raise_user_error('duplicated move number',
+                    self.raise_user_error('number exists',
                                           {'move_number' : move.number})
-                    print("duplicated move")
-                    continue
 
                 move.date = iline.fecha
                 move.period = Period.find(Transaction().context.get('company')
@@ -151,26 +168,42 @@ class AccountImportContaplus(Wizard):
 
             accounts = Account.search([('code', '=', account)], limit=1)
             if not accounts:
-                # self.raise_user_error()
-                print("missing account %s" % account)
-                continue
+                self.raise_user_error('account not found', {'account': account})
+                # print('account "%s" not found' % account)
+                # continue
 
             line.account = accounts[0]
             if party:
                 parties = Party.search([('rec_name', 'ilike', '%' + party)], limit=2)
                 if (not parties) and (len(parties > 1)):
-                    # self.raise_user_error()
-                    print("error party: %s" % party )
-                    continue
+                    self.raise_user_error('party not found', {'party': party})
+                    # print("no party found")
+                    # continue
 
                 line.party = parties[0]
 
-            line.debit = iline.euro_debe
-            line.credit = iline.euro_haber
+            # swap debe haber in some cases due to error.
+            # in caja the concepto/clave determines if it is debe or haber.
+            if iline.concepto.strip in ('',
+                                        'TALON RTTE',
+                                        'CLAVE MANUAL',
+                                        'PAGO ITV',
+                                        'DESEMBOLSO',
+                                        'TRASP. A BANC',
+                                        'ANTICP-VALES'):
+                line.debit = iline.euro_haber + iline.euro_debe
+                line.credit = 0
+            else:
+                line.debit = iline.euro_debe
+                line.credit = iline.euro_haber
+
             line.description = " ".join([iline.concepto, iline.documento])
 
             move.lines = move.lines + (line,)
 
+        unbalance_moves = filter(not_balance, to_create.values())
+        if (unbalance_moves):
+            self.raise_user_error('unbalance lines')
         if to_create:
             Move.save(to_create.values())
         return 'end'
