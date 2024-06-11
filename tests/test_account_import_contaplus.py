619754462d87b9fdebcc80e0685cd41195bbbe49
Raimon Esteve <raimonesteve@gmail.com>
2024-06-11 07:14:40 +0200

    Add test support account ES taxes

--- tests/test_account_import_contaplus.py
+++ tests/test_account_import_contaplus.py
@@ -2,18 +2,26 @@
 # this repository contains the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
+from trytond.pool import Pool
+from decimal import Decimal
 
 
 class AccountImportContaplusTestCase(ModuleTestCase):
     'Test Account Import Contaplus module'
     module = 'account_import_contaplus'
 
+    @with_transaction()
+    def test_account_es(self):
+        'Test taxes from account_es'
+        pool = Pool()
+        ModelData = pool.get('ir.model.data')
+        TaxTemplate = pool.get('account.tax.template')
 
-def suite():
-    suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        AccountImportContaplusTestCase))
-    return suite
+        t_vat_21 = TaxTemplate(ModelData.get_id('account_es', 'iva_rep_21'))
+        t_vat_0 = TaxTemplate(ModelData.get_id('account_es', 'iva_rep_ex'))
+        self.assertEqual(t_vat_21.rate * 100, Decimal('21.00'))
+        self.assertEqual(t_vat_0.rate * 100, Decimal(0))
+
 
 del ModuleTestCase
